71f97b5845f0e4d672fd978252fb50152d3772b1
ShevchukA <antoshka.vbg@mail.ru>
2023-06-29 06:08:30 +0300

    feat: add scrolling to tranding section

--- src/components/TrendLine.js
+++ src/components/TrendLine.js
@@ -2,6 +2,7 @@ import classes from "./TrendLine.module.css";
 import data from "../data.json";
 
 function TrendLine({ children }) {
+  console.log("TRENDING");
   const trendingContent = data
     .filter((item) => item.isTrending)
     .map((foundItem) => (
@@ -11,10 +12,18 @@ function TrendLine({ children }) {
       />
     ));
 
+  function scrollHandler(e) {
+    // console.log(e.deltaY);
+    const line = e.target.closest("div"); // select whole div
+    line.scrollLeft += e.deltaY; //deltaY because we rotate mousewheel down or up
+  }
+
   return (
     <section>
       <h1>Trending</h1>
-      <div className={classes.line}>{trendingContent}</div>
+      <div className={classes.line} onWheel={scrollHandler}>
+        {trendingContent}
+      </div>
     </section>
   );
 }
